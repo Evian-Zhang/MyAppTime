e46167876b99ffb8b7062328ba7605b0c74cf722
Evian-Zhang <evianzhang1999@163.com>
2019-03-25 23:42:56 +0800

    Initial Commit

---
+++ MyAppTime/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  MyAppTime
+//
+//  Created by Evian张 on 2019/3/25.
+//  Copyright © 2019 Evian张. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+
+@end
+
---
+++ MyAppTime/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MyAppTime
+//
+//  Created by Evian张 on 2019/3/25.
+//  Copyright © 2019 Evian张. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
